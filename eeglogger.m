e1b2da521456a725ba048d539102deb917e6fead
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2014-11-24 12:55:29 +1300

    removed addheader in loadlibrary

--- eeglogger.m
+++ eeglogger.m
@@ -81,7 +81,7 @@ elseif optargin == 1
 end
 % Check to see if library was already loaded
 if ~libisloaded('edk')    
-    [nf, w] = loadlibrary('edk','edk',  'addheader', 'EmoStateDLL', 'addheader', 'edkErrorCode'); 
+    [nf, w] = loadlibrary('edk','edk','EmoStateDLL','edkErrorCode'); 
 	disp(['EDK library loaded']);
     if( lib_flag_popup )
         libfunctionsview('edk')
