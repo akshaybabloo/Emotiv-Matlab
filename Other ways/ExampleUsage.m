c133b99990296612fe1e462d21e1c17acf8c1739
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2014-11-24 18:58:43 +1300

    Other ways of retrieving eeg signals from emotiv

---
+++ Other ways/ExampleUsage.m
@@ -0,0 +1,20 @@
+function [ ] = ExampleUsage()
+h = EmotivEEG;
+h.Run
+
+for i = 1:10
+    data_local = h.data;
+    data_local = data_local+1;
+    surf(data_local);
+    pause(0.5);
+end
+
+
+lastfilename = h.Record(10);
+h.LoadRecordedData(lastfilename)
+data_local = h.data;
+plot(data_local);
+
+delete(h);
+end
+
