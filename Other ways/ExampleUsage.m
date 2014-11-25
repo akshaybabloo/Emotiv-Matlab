e627d8f03f4ca7b8642146432d90fa0283e6d007
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2014-11-25 14:40:45 +1300

    Commented part of code
    % lastfilename = h.Record(10);
    % h.LoadRecordedData(lastfilename)
    % data_local = h.data;
    % plot(data_local);

--- Other ways/ExampleUsage.m
+++ Other ways/ExampleUsage.m
@@ -2,18 +2,18 @@ function [ ] = ExampleUsage()
 h = EmotivEEG;
 h.Run
 
-for i = 1:10
+for i = 1:5
     data_local = h.data;
-    data_local = data_local+1;
-    surf(data_local);
+    data_local = (data_local+1)*10;
+    plot(data_local);
     pause(0.5);
 end
 
 
-lastfilename = h.Record(10);
-h.LoadRecordedData(lastfilename)
-data_local = h.data;
-plot(data_local);
+% lastfilename = h.Record(10);
+% h.LoadRecordedData(lastfilename)
+% data_local = h.data;
+% plot(data_local);
 
 delete(h);
 end
